d614ba68cc8811a8878bec73d82f3209849b24bb
dmitriy.b <dmitriy.b@smplt.net>
2022-03-09 16:19:34 +0500

    Fix GRPC models

--- src/Service.PaymentDeposit.Domain.Models/IPaymentProviderGrpcService.cs
+++ src/Service.PaymentDeposit.Domain.Models/IPaymentProviderGrpcService.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
 
 namespace Service.PaymentDeposit.Domain.Models
 {
+	[ServiceContract]
 	public interface IPaymentProviderGrpcService
 	{
+		[OperationContract]
 		ValueTask<ProviderDepositGrpcResponse> DepositAsync(ProviderDepositGrpcRequest request);
 	}
 }

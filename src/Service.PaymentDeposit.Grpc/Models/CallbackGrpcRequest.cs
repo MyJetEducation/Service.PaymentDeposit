a7bbad38050f2e20e33b125f89c35f1ccf67ba48
dmitriy.b <dmitriy.b@smplt.net>
2022-03-10 11:58:30 +0500

    Change contract

--- src/Service.PaymentDeposit.Grpc/Models/CallbackGrpcRequest.cs
+++ src/Service.PaymentDeposit.Grpc/Models/CallbackGrpcRequest.cs
@@ -8,15 +8,12 @@ namespace Service.PaymentDeposit.Grpc.Models
 	public class CallbackGrpcRequest
 	{
 		[DataMember(Order = 1)]
-		public Guid? UserId { get; set; }
-		
-		[DataMember(Order = 2)]
 		public Guid? TransactionId { get; set; }
 		
-		[DataMember(Order = 3)]
+		[DataMember(Order = 2)]
 		public string ExternalId { get; set; }	
 		
-		[DataMember(Order = 4)]
+		[DataMember(Order = 3)]
 		public TransactionState State { get; set; }
 	}
 }

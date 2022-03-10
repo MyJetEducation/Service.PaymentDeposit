0bb53ee44935ecf011c8a73fc3cbe75967e71aa7
dmitriy.b <dmitriy.b@smplt.net>
2022-03-10 17:51:48 +0500

    Fix contract

--- src/Service.PaymentDeposit.Domain.Models/ProviderDepositGrpcResponse.cs
+++ src/Service.PaymentDeposit.Domain.Models/ProviderDepositGrpcResponse.cs
@@ -6,10 +6,13 @@ namespace Service.PaymentDeposit.Domain.Models
 	[DataContract]
 	public class ProviderDepositGrpcResponse
 	{
+		[DataMember(Order = 1)]
 		public TransactionState State { get; set; }
 
+		[DataMember(Order = 2)]
 		public string ExternalId { get; set; }
 
+		[DataMember(Order = 3)]
 		public string RedirectUrl { get; set; }
 	}
 }
